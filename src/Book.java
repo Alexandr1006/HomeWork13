f43890cc61987a8fb40a453e0235a6c876e72fca
Александр <Alexander.popov.usurt@mail.ru>
2022-11-23 01:24:05 +0500

    HomeWork13

---
+++ src/Book.java
@@ -0,0 +1,24 @@
+public class Book {
+   private final String name;
+   private final Author author;
+   private int yearEdition;
+
+   public Book (String name, int yearEdition, Author author){
+      this.name = name;
+      this.yearEdition = yearEdition;
+      this.author = author;
+   }
+      public String getName(){
+      return this.name;
+      }
+      public void setyearEdition(int yearEdition){
+       this.yearEdition = yearEdition;
+      }
+      public int getYearEdition(){
+      return this.yearEdition;
+      }
+
+      public Author getAuthor() {
+      return this.author;
+   }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author = new Author ("Александр", "Дюма");
+        Author author1 = new Author("Герберт", "Уэ́ллс");
+        Book book = new Book("Три мушкетера",1844, author);
+        Book book1 = new Book("The Invisible Man", 1897, author1);
+
+        System.out.println(author.getNameAuthor() + " " + author.getSurnameAuthor() + " " + book.getName() + " " +  book.getYearEdition());
+        System.out.println(author1.getNameAuthor() + " " + author1.getSurnameAuthor() + " " + book1.getName() + " " + book1.getYearEdition());
+        book.setyearEdition(1944);
+        System.out.println(book.getYearEdition());
+
     }
 }
